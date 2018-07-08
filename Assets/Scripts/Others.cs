2c1937885d0a09dbef69838a1c19535f150a0e0b
Daniel Henrique <danielshenrique@gmail.com>
2018-07-07 22:05:01 -0300

    4
    4

--- Assets/Scripts/Others.cs
+++ Assets/Scripts/Others.cs
@@ -1,8 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Others : MonoBehaviour {
 
-   
+
+	private GameObject others;
+
+	private void Start()
+	{
+		others = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	private void Update()
+	{
+
+		if (gameObject.GetComponent<Static>().Pegador == true)
+		{
+			GetComponent<NavMeshAgent>().destination = others.transform.position;
+		}
+
+	}
 }
