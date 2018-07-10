605621eb34526dddfde737311327b1e89812e8c3
helenacurvello <leninhacurvello@gmail.com>
2018-07-10 11:51:35 -0300

    Mov
    Jogadores andando sozinhos

---
+++ Assets/Scripts/Mov.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mov : MonoBehaviour {
+
+	public float vel;
+	public float pontoA;
+	public float pontoB;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate(vel * Time.deltaTime, 0, 0);
+		if (transform.position.x >= pontoA || transform.position.x <= pontoB)
+		{
+			vel *= -1;
+		}
+	}
+}
+
